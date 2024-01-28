78be8af684f20421cd1e35350c44de3e279c378f
BFipe <ilya.maximov11@gmail.com>
2024-01-28 09:39:25 +0300

    Done Image input and saving to localstorage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import dynamic from "next/dynamic";
 import ImageRecreator from "@/components/ImageRecreator/ImageRecreator";
 const ImageInput = dynamic(() => import("@/components/ImageInput/ImageInput"));
